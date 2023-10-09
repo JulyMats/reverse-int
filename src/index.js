4056ceecd77efbea9eff4dd82b3f913084c6ae2e
Matsukevich Yuliya <july.matsukevich@mail.ru>
2023-10-09 22:49:10 +0300

    add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+  let result = '';
+  let arr = Array.from(String(n));
+
+  if (arr[0] === '-') {
+    arr.shift();
+  }
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result += arr[i];
+  }
   
+  return Number(result);
 }
